1961e1beb78f9de2910c63fe118ef137849769ea
Enrico Brambilla <enric.brambilla@gmail.com>
2022-02-25 11:17:10 +0100

    Aggiunti diagrammi delle classi parziali

--- DOC/watch_gen_pdf.sh
+++ DOC/watch_gen_pdf.sh
@@ -5,7 +5,7 @@ do
     chsum2=`find ./ -name "*.tex" -type f -exec md5sum {} \;`
     if [[ $chsum1 != $chsum2 ]] ; then           
         if [ -n "$chsum1" ]; then
-            sh ./gen_pdf.sh 2
+            sh ./gen_pdf.sh 1
         fi
         chsum1=$chsum2
     fi
