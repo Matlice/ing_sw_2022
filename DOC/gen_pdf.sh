b7b96560a13d0cc2dfa8517d304af3b3399646ac
Stefano Fontana <stefano.fontana.2000@gmail.com>
2022-02-25 21:17:58 +0100

    puml compilation in a separate folder. documentation.

--- DOC/gen_pdf.sh
+++ DOC/gen_pdf.sh
@@ -9,6 +9,21 @@ export PLANTUML_JAR=./bin/plantuml.jar
 
 TIMES=1
 if [ $# -eq 1 ]; then
+    if [ $1 == 'clean' ]; then
+        rm -r compiled-puml
+        find . -name '*.aux' -exec rm -v {} \;
+        find . -name '*.toc' -exec rm -v {} \;
+        find . -name '*.pdf' -exec rm -v {} \;
+        find . -name '*.lof' -exec rm -v {} \;
+        find . -name '*.lot' -exec rm -v {} \;
+        find . -name '_markdown_main/' -exec rm -v {} \;
+        find . -name '*.md.tex' -exec rm -v {} \;
+        find . -name '*.markdown.lua' -exec rm -v {} \;
+        find . -name '*.fdb_latexmk' -exec rm -v {} \;
+        find . -name '*.fls' -exec rm -v {} \;
+        find . -name '*.pytxcode' -exec rm -v {} \;
+        exit
+    fi
     TIMES=$1
 fi
 
